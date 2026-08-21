d53f75f58413aae8c1d13f55ef0d62345a16dd12
Anton-Barash <anton.barash@gmail.com>
2026-08-21 16:17:35 +0800

    chore: remove all built web assets and intermediate build files
    This commit deletes the entire pre-built web output directory contents including:
    - Flutter web build artifacts like main.dart.js part files
    - Static assets (images, fonts, ffmpeg binaries)
    - Service worker and manifest files
    - Generated shader and build metadata files
    
    This cleans up the repository to only contain source code, ready for a fresh production build.

--- web/main.dart.js_17.part.js
+++
@@ -1,6 +0,0 @@
-((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
-A=c[0]
-B=c[31]
-var z=a.updateTypes([]);(function constants(){B.NN=new A.z(!0,null,null,null,null,null,16,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null)})()};
-(a=>{a["fMIiRs7GDYW4xCY869Yv8cVqDoU="]=a.current})($__dart_deferred_initializers__);
